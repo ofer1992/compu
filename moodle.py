90ba9e99aa78157b106828cad0eb49a79f5a011b
Ofer Yehuda <ofer1992@cs.huji.ac.il>
2017-06-08 15:04:09 +0300

    big breakthrough

--- moodle.py
+++ moodle.py
@@ -1,7 +1,10 @@
 # Trying to upload a file to moodle
 
 import requests
-import webbrowser
+import bs4
+import re
+
+
 
 #Login to moodle
 def login(user,pw):
@@ -9,32 +12,43 @@ def login(user,pw):
     session = requests.session()
     res = session.post(loginPage, {"username": user, "password":
 pw})
-    return session, res
+    return session
 
-def page(res):
-    open('temp.htm','w').write(res.text)
-    webbrowser.open('temp.htm')
+# Build confirm form submission
+def confirm(tags,itemid):
+    forms = {}
+    for tag in tags:
+        forms[tag.get('name')] = tag.get('value')
+    forms['files_filemanager'] = itemid
+    forms['submitbutton'] = 'Save+changes'
+    return forms
 
-def get_sesskey(res):
-    i = res.text.find("sess")
-    if i == -1: return "FAIL"
-    return res.text[i+10:i+20]
 
-def upload(f,sess, key):
-    url = "https://moodle2.cs.huji.ac.il/nu16/repository/filepicker.php?ctx_id=446183&itemid=517031988&env=filemanager&course=80428&maxbytes=10485760&areamaxbytes=-1&maxfiles=1&subdirs=1&sesskey="+key+"&action=browse&draftpath=%2F&savepath=%2F&repo_id=4"
-
-    sess.get("https://moodle2.cs.huji.ac.il/nu16/repository/draftfiles_manager.php?ctx_id=446183&itemid=517031988&env=filemanager&course=80428&maxbytes=10485760&areamaxbytes=-1&maxfiles=1&subdirs=1&sesskey="+key+"&action=deletedraft&draftpath=%2F&filename=infi8.pdf")
-    res = sess.post(url,files={'repo_upload_file': f})
+# TODO: Add case where there is already a file present
+# Receives session, id of upload page and file to be uploaded
+def upload(s,id,f):
+    res = s.get("https://moodle2.cs.huji.ac.il/nu16/mod/assign/view.php?id="+id+"&action=editsubmission") # edit submission page
+    bs = bs4.BeautifulSoup(res.text)
+    data = bs.select('noscript object')[0].get('data') # extracts the file manager
+    data = data.replace('&amp;','&')
     
-    forms = {'lastmodified': '1496337956',
-            'id': '220795',
-            'userid': '442422',
-            'action': 'savesubmission',
-            'sesskey': key,
-            '_qf__mod_assign_submission_form': '1',
-            'files_filemanager': '517031988',
-            'submitbutton': 'Save+changes'}
-
-    page(sess.post("https://moodle2.cs.huji.ac.il/nu16/mod/assign/view.php", forms))
-    return res
-
+    # Here we extract data later to be used for the confirm upload POST
+    tags = bs.select('form.mform > div input')
+    m = re.search(r'itemid=(\d*)',data)
+    itemid = m.group(1)
+    forms = confirm(tags,itemid)
+    
+    # Here we get the enter the file submission page, extract the filepicker url and modify it to upload our file
+    res = s.get(data)
+    bs = bs4.BeautifulSoup(res.text)
+    data = bs.select('div.filemanager-toolbar a')[0].get('href') # getting the upload url
+    data = data.replace('&amp;','&')
+    data = data.replace('action=plugins', 'action=upload')
+    data += "&repo_id=4"
+    s.post(data,files={'repo_upload_file': f})
+    
+    # Confirm upload POST
+    s.post("https://moodle2.cs.huji.ac.il/nu16/mod/assign/view.php?", forms)
+    
+    
+    
